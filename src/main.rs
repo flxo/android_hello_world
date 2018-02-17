9103401ecd8f2b9b870577c6912e96e8dd7f9a69
Felix Obenhuber <felix@obenhuber.de>
2018-02-17 19:46:31 +0100

    Add android logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate android_logger;
+#[macro_use]
+extern crate log;
+
 fn main() {
-    println!("hello world!");
+    ::android_logger::init_once(log::LogLevel::Trace);
+    loop {
+        println!("hello world!");
+        debug!("hello world!");
+        std::thread::sleep(std::time::Duration::new(1, 0));
+    }
 }
